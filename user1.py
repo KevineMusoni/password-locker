d58899a648319e5c82fbc3148b0d5e04fc3ac711
Kevine Musoni <kevinenicky@gmail.com>
2018-11-02 14:42:15 +0300

    initialisation

--- user1.py
+++ user1.py
@@ -7,45 +7,45 @@ class User:
     field = input("Enter Password:\n")
     detailList=field
     print(field)
-#   detailList = []
+  detailList = []
 def __init__(self, user_name, password):
     self.user_name = user_name
     self.password = password
 @classmethod
-def save_credentials(self):
-    User.detailList.append(self)
-@classmethod
-def display_users(cls):
-    return cls.detailList
+# def save_credentials(self):
+#     User.detailList.append(self)
+# @classmethod
+# def display_users(cls):
+#     return cls.detailList
 
 
-    class Credentials:
-        credential_list =[]
-def check_user_exist(cls,user_name,password):
+#     class Credentials:
+#         credential_list =[]
+# def check_user_exist(cls,user_name,password):
     
-    for user in User.detailList:
-      if user.user_name == user_name and user.password == password:
-        return True
-      return False
+#     for user in User.detailList:
+#       if user.user_name == user_name and user.password == password:
+#         return True
+#       return False
 
   
-def save_account(self):
+# def save_account(self):
     
-    Credentials.credential_list.append(self)
+#     Credentials.credential_list.append(self)
 
-def delete_account(self):
+# def delete_account(self):
     
-    Credentials.credential_list.remove(self)
+#     Credentials.credential_list.remove(self)
 
-@classmethod
-
-
-#   def generate_password
+# @classmethod
 
 
-def display_accounts(cls):
+# #   def generate_password
+
+
+# def display_account(cls):
     
-    return cls.credential_list
+#     return cls.credential_list
   
 
 
