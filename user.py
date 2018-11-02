113c4eaf64a96d8787ccad993ddbc94c05ea082f
Kevine Musoni <kevinenicky@gmail.com>
2018-11-02 13:46:04 +0300

    credentials input

---
+++ user.py
@@ -0,0 +1,12 @@
+def passwordRequest():
+    field = input("Enter Password:\n")
+    password=field
+    print(field)
+    if field==password:
+        return True
+        return False
+if passwordRequest():
+print("successfully logged in")
+else:
+    
+
