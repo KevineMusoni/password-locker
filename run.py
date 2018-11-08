663e9d59d6ed14069d5112026bd235151b55cf1a
Kevine Musoni <kevinenicky@gmail.com>
2018-11-08 06:51:33 +0300

    run.py file updated

---
+++ run.py
@@ -0,0 +1,121 @@
+from user_1 import User, Credentials
+def function():
+	print(" __      __   ___ ____  __                      ")
+	print("|  |    |  | |            |   \            /    |")
+	print("|  |    |  | |            |    \          /     |")
+	print("|  |    |  | |            |     \        /      |")
+	print("|  |    |  | |__ ___ ___ _|      \      /       |")
+	print("|  |____|  | |                    \    /        |")
+	print("|          | |                     \  /         |") 
+	print("|   ____   | |                      ||          |")
+	print("|  |    |  | |                      ||          |")
+	print("|  |    |  | |                      ||          |")
+	print("|  |    |  | |                      ||          |")
+	print("|__|    |__| |__ __ __ ___          ||          |")
+	print("                                                0")
+	
+function()
+
+def createUser(user_name, password):
+  new_user = User(user_name,password)
+  return new_user
+
+def saveUserDetails(user):
+  user.user_save()
+def displayUsers():
+   return User.display_users()
+
+def loginUser(user_name,password):
+  validation = Credentials.validation(user_name,password)
+  return validation
+
+def createCredential(account_name,account_username, account_password):
+  newCredential = Credentials(account_name,account_username,account_password)
+  return newCredential
+
+def saveCredential(credential):
+  credential.saveAccount()
+
+def deleteAccount(credential):
+  credential.deleteAccount()
+
+def displayAccounts():
+  return Credentials.displayAccounts()
+  
+def generatePassword():
+  passwordGenerate = Credentials.generatePassword()
+  return passwordGenerate
+
+def main():
+  print("Welcome to Password Locker!")
+  while True:
+    cmd_i_ = input("Type create to create a new user account, and q to quit")
+    
+if cmd_i == "q":
+        print("Thank you for for using password locker! ")
+
+elif cmd_i == "create":
+        print("Sign Up")
+        user_name = input("User_name: ")
+        password = input("Password: ")
+        saveUserDetails(create_user(user_name,password))
+        print('\n')
+        
+        print(f"succesfully created...")
+        
+elif cmd_i == "show":
+        if displayUsers():
+            print("Below is the list of users:")
+            print('\n')
+            for user in displayusers():
+                print(f"{user.user_name}")
+                print('\n')
+        else:
+            print("data not found")
+                
+        
+elif cmd_i == "login":
+        print("Enter your User name and your Password to log in:")
+        user_name = input("User name: ")
+        password = input("password: ")
+        sign_in = login_user(user_name,password)
+        if sign_in == True:
+            print(f"Welcome to Passsword Locker!")
+            while True:
+                cmd_i = input("Type create to create an account show to display the list of your accounts, q to quit \n")
+                if cmd_i == "create":
+                    print("Create new account")
+                    account_name = input("name: ")
+                    account_username = input("Site Name: ")
+                    password_option = input("Please choose between: (existing password) or (new password) \n").strip()
+                    while True:
+                        if password_option == "existing password":
+                            account_password = input("Enter your password: (minimum 8 characters): ")
+                            break
+                        elif password_option == "new password":
+                            account_password = generatePassword()
+                            break
+                        else:
+                            print("Invalid!")
+                            break
+                    save_credential(create_credential(account_name,account_username,account_password))
+                    print(f"New created account: \n Account:{account_name}\n User Name:{account_username} \n Password: {account_password}")
+    
+                elif cmd_i == "show":
+                    if displayAccounts():
+                        print("List:")
+                        for account in displayAccounts():
+                            print(f" Site:{account.account_name} \n User Name:{account_username}\n Password:{account_password}")
+                    else:
+                        print("Sorry,......you dont have any accounts yet")
+                        
+                elif cmd_i == "q":
+                    print("Thank you for for using password locker! ")
+                    break
+                else:
+                    print("Not a known command")
+        else:
+            print("Create an Account")
+
+if __name__ == '__main__':
+  main()
---
+++ user_1.py
@@ -0,0 +1,56 @@
+import random
+import string
+
+# user class
+
+class User:
+
+  userList = []
+  def __init__(self, user_name, password):
+    self.user_name = user_name
+    self.password = password
+  
+  def saveUser(self):
+    User.userList.append(self)
+
+  @classmethod
+  def displayAllUsers(cls):
+    return cls.userList
+
+# credentials class
+class Credentials:
+  credentialList =[]
+  def __init__(self, account_name, account_username, account_password):
+    self. account_name = account_name
+    self.account_username = account_username
+    self.account_password = account_password
+
+  @classmethod
+  def validation(cls,user_name,password):
+    for user in User.userList:
+      if user.user_name == user_name and user.password == password:
+        return True
+      return False
+
+  
+  def save(self):
+    Credentials.credentialList.append(self)
+
+  def delete(self):
+    Credentials.credentialList.remove(self)
+
+  @classmethod
+  def displayAllAccounts(cls):
+    return cls.credentialList
+  def generatePassword():
+    gen_pass = "".join(random.choice(char) for _ in range(10))
+    return gen_pass
+#   def generatePassword(num):
+#     password=''
+
+#     for x in range(num):
+#       y = random.randint(0,50)
+#       password += string.printable[x]
+#       #string.printable[] is a combination of digits, ascii_letters, punctuation, and whitespace for password input.
+#     return password
+# print (generatePassword)
